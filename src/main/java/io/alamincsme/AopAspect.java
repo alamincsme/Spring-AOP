01b934e8395cc00d52744e10d01f7df3086ccdb2
alamincsme <alamincsme@gmail.com>
2024-02-22 15:59:08 +0600

    Point cut before and after implementation

--- src/main/java/io/alamincsme/AopAspect.java
+++ src/main/java/io/alamincsme/AopAspect.java
@@ -1,6 +1,33 @@
 package io.alamincsme;
 
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
 public class AopAspect {
 
+    Logger logger = LoggerFactory.getLogger(AopAspect.class);
 
+
+
+//    @Before("execution(* AopClass+.*(..))")
+    @Before("execution(* AopClass+.*(..))")
+    public void before (JoinPoint joinPoint) {
+        logger.info("Starting ..........."+ joinPoint.getSignature().getName() );
+        logger.info("with parameter : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @After("execution(* AopClass+.*(..))")
+    public void after (JoinPoint joinPoint) {
+        logger.info("..........fininshed...... : " + joinPoint.getSignature().getName());
+    }
 }
